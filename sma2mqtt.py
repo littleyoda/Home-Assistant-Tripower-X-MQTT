db3212c97535fb5c7185ff6f32809df17f4c2e69
Sven Bursch-Osewold <sb_git@bursch.com>
2023-04-15 08:04:17 +0200

    Update to ha_mqtt_discoverable 0.8.x Breacking Change: Device name and Sensor names within Home Assistant will change

--- sma2mqtt.py
+++ sma2mqtt.py
@@ -1,4 +1,5 @@
-from ha_mqtt_discoverable.device import Device
+from ha_mqtt_discoverable import Settings
+from ha_mqtt_discoverable.sensors import SensorInfo, Sensor, DeviceInfo
 import json
 import time
 import requests
@@ -34,7 +35,7 @@ def unit_of_measurement(name):
         return "V"
     if (name.endswith(".VA.")):
         return "VA"
-    logging.debug("No unit of measurement for " + name)    
+    logging.debug("No unit of measurement for " + name)
     return ""
 
 def isfloat(num):
@@ -44,7 +45,20 @@ def isfloat(num):
     except ValueError:
         return False
 
-
+sensors = {}
+def sendUpdate(name, uofm, mqtt_settings, device_info, value):
+    sensor = sensors.get(name)
+    if (sensor is None):
+        sensor_info = SensorInfo(unit_of_measurement=uofm, 
+                                name=name, 
+                                device_class=None, 
+                                unique_id=dev["serial"]+"-" + name, 
+                                device=device_info)
+        sensor = Sensor(Settings(mqtt=mqtt_settings, entity=sensor_info))
+        sensors[name] = sensor
+        time.sleep(0.1)
+    print(">>>>>> " + name + " " + str(value))
+    sensor.set_state(value)
 
 
 with open('sma2mqtt.yaml') as f:
@@ -75,21 +89,20 @@ url="http://" + cfg["InverterAdress"] + "/api/v1/plants/Plant:1/devices/IGULD:SE
 x = requests.get(url, headers = headers)
 dev = x.json()
 
-# Configure Device for ha_mqtt_discoverable
-configd = {
-    "mqtt_server": cfg["MqttServer"],
-    "mqtt_prefix": cfg["MqttPrefix"],
-    "mqtt_user": cfg["MqttUser"],
-    "mqtt_password": cfg["MqttPassword"],
-    "device_id": dev["serial"],
-    "device_name": dev["product"],
-    "device_class":"None",
-    "manufacturer": dev["vendor"],
-    "model": dev["vendor"]+"-" + dev["product"],
-    "client_name": "sma2mqtt" + str(dev["serial"]),
-    "unique_id": dev["vendor"]+"-" + dev["product"] + "-" + str(dev["serial"])
-}
-device = Device(settings=configd)
+# Setup MQTT
+mqtt_settings = Settings.MQTT(host=cfg["MqttServer"], 
+                              username=cfg.get("MqttUser"), 
+                              password=cfg.get("MqttPassword"),
+                              discovery_prefix=cfg.get("MqttPrefix", "homeassistant") )
+device_info = DeviceInfo(name=dev["product"], 
+                         configuration_url='http://' + cfg["InverterAdress"], 
+                         identifiers=dev["serial"], 
+                         model = dev["vendor"]+"-" + dev["product"],
+                         manufacturer = dev["vendor"],
+                         sw_version = dev['firmwareVersion'])
+
+
+
 
 exitafter = cfg.get("ExitAfter", None)
 while exitafter is None or exitafter > 0:
@@ -114,30 +127,19 @@ while exitafter is None or exitafter > 0:
                 v = d["values"][0]["value"]
                 if isfloat(v):
                     v = round(v,2)
-                device.add_metric(
-                    name= dname,
-                    value=v,
-                    configuration={"name": dname},
-                    unit_of_measurement = unit_of_measurement(dname)
-                )
+                sendUpdate(cfg.get("InverterPrefix", "") + dname, unit_of_measurement(dname), mqtt_settings, device_info,v);
             elif "values" in d["values"][0]:
                 for idx in range(0, len(d["values"][0]["values"])):
                     v = d["values"][0]["values"][idx]
                     if isfloat(v):
                         v = round(v,2)
                     idxname = dname + "." + str(idx + 1)
-                    device.add_metric(
-                        name= idxname,
-                        value=v,
-                        configuration={"name": idxname} ,
-                        unit_of_measurement = unit_of_measurement(dname)
-                    )
+                    sendUpdate(cfg.get("InverterPrefix", "") + idxname, unit_of_measurement(dname), mqtt_settings, device_info,v);
             else:
+ 
                 # Value current not available // night?
                 pass
 
-        logging.info("Publishing to Home Assistant")
-        device.publish()
         time.sleep(cfg["UpdateTimeSec"])
     except TimeoutError:
         pass
